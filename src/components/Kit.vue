966210804754980dddb57c69831b250480171a26
Zachary K Watkins <3458875+ZachWatkins@users.noreply.github.com>
2021-12-29 16:52:59 -0600

    Reset the VueJS project using new configurations

--- src/components/Kit.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<h3>{{ name }} #{{ id }}</h3>
-	<ul>
-		<li><a href="https://www.google.com/search?q=lego+kit+{{ id }}" target="_blank">Search for images of this kit on Google</a></li>
-	</ul>
-</template>
