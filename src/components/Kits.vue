42d4afe1d8feeeef62f42d290a999b4576c73052
Zachary K Watkins <3458875+ZachWatkins@users.noreply.github.com>
2021-12-31 17:24:05 -0600

    Make much progress towards configuring the application properly

---
+++ src/components/Kits.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Check out the lego kits we've finished!
+    </p>
+    <h3>Kits</h3>
+    <ul id="kit-list">
+      <li v-for="item in items" :key="item.id">#{{ item.id }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import legoKits from '../data/lego-kits.json'
+
+/* exported kitList */
+var kitItems = new Vue({ // eslint-disable-line no-new
+  el: '#kit-list',
+  data () {
+    return {
+      items: legoKits
+    }
+  }
+})
+export default {
+  name: 'Kits',
+  props: {
+    msg: String,
+    items: kitItems
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Kits msg="Lego Kit Library" />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Kits from '@/components/Kits.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Kits
+  }
+}
+</script>
