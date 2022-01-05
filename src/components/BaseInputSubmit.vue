aa2a81eab2b0a64477a1ff25155157cfdd3c4721
Zachary K Watkins <3458875+ZachWatkins@users.noreply.github.com>
2022-01-04 23:08:07 -0600

    Create a basic submit button component

---
+++ src/components/BaseInputSubmit.vue
@@ -0,0 +1,21 @@
+<template>
+  <input type="submit" :value="value" />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+input[type="submit"]{
+  padding: 8px;
+  font-size: 18px;
+}
+</style>
